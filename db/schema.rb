1f6778cf9f1a69d9cb61d6ac7fadf21ad0ea4846
WozniakMac <maciekwo@gmail.com>
2015-05-23 16:07:45 +0200

    Add name to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523125710) do
+ActiveRecord::Schema.define(version: 20150523135116) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150523125710) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "points"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
