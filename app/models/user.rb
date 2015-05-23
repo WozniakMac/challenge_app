1ad5c9bbacc53b1229fb5f307bebb5892045172d
WozniakMac <maciekwo@gmail.com>
2015-05-23 17:24:39 +0200

    Add avatars to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   # :recoverable, :rememberable and :trackable
   devise :database_authenticatable, :registerable, :validatable
 
+  has_attached_file :avatar, :styles => { :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   has_many :questions
   has_many :answers
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523135116) do
+ActiveRecord::Schema.define(version: 20150523150439) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -46,12 +46,16 @@ ActiveRecord::Schema.define(version: 20150523135116) do
   add_index "questions", ["user_id"], name: "index_questions_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "points"
     t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
