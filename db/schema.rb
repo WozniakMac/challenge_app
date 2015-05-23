e41af878a31befcca9e66d3208ddb274ed435069
WozniakMac <maciekwo@gmail.com>
2015-05-23 14:58:22 +0200

    Add points to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522214407) do
+ActiveRecord::Schema.define(version: 20150523125710) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150522214407) do
     t.string   "encrypted_password", default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "points"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
