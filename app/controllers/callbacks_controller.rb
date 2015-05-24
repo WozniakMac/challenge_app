bd43c3ba95441738f545e27659d80add0870a39f
WozniakMac <maciekwo@gmail.com>
2015-05-24 03:57:36 +0200

    Logger rwpair

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,7 +2,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
     def github
         @user = User.from_omniauth(request.env["omniauth.auth"])
         logger.debug "User created"
-        logger.debug "#{user.email}"
+        logger.debug "#{@user.email}"
         sign_in_and_redirect @user
     end
 end
