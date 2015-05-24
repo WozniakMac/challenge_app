71cd3869b322431e586ee23325f4fd5dbf3a1b9d
WozniakMac <maciekwo@gmail.com>
2015-05-24 10:29:37 +0200

    Add more loggers

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,8 +1,10 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     def github
+    		logger.debug "Start github"
         @user = User.from_omniauth(request.env["omniauth.auth"])
         logger.debug "User created"
         logger.debug "#{@user.email}"
         sign_in_and_redirect @user
+        logger.debug "End github"
     end
 end
