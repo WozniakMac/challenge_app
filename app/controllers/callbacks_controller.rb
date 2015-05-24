b6bd46b066adb3cf92c786cb95195b6018e1b2f4
WozniakMac <maciekwo@gmail.com>
2015-05-24 10:38:48 +0200

    Test github

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     def github
-    		logger.debug "Start github"
         @user = User.from_omniauth(request.env["omniauth.auth"])
-        logger.debug "User created"
-        logger.debug "#{@user.email}"
+        p @user
         sign_in_and_redirect @user
-        logger.debug "End github"
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,12 +34,10 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
-        logger.debug "#{auth.provider}"
         user.uid = auth.uid
-        logger.debug "#{auth.uid}"
         user.email = auth.info.email
-        logger.debug "#{auth.info.email}"
         user.password = Devise.friendly_token[0,20]
+        user.save
       end
   end
 end
