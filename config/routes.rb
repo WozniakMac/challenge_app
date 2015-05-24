ecacafce181f0c6844c70cb0678d3eb235ad677f
WozniakMac <maciekwo@gmail.com>
2015-05-24 12:13:11 +0200

    Refactoring initialization users points

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { :registrations => "registrations", :omniauth_callbacks => "callbacks" }
+  devise_for :users, controllers: { :omniauth_callbacks => "callbacks" }
   root to: 'questions#index'
 
   resources :questions do
